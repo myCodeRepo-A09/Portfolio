95554a42d8dc3654bd6994cd042f813e0153cdf1
Avinash <avinash.andhale@tcs.com>
2025-08-10 12:40:21 +0530

    env chatsocket

--- devavi-dashboard/src/app/core/environments/environment.ts
+++ devavi-dashboard/src/app/core/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  chatSocketUrl: 'http://13.223.184.233/:4000',
+  chatSocketUrl: 'http://13.223.184.233:4000',
   chatAttachments: false,
   learnWithMeSection: false,
 };
